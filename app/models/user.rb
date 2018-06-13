768611e48da948fbb21dc1f9406ebe59233cc7d3
Franco Rivera <francoriverarivas@hotmail.com>
2018-06-13 09:24:53 -0500

    added basic jwt implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# app/models/user.rb
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # Model associations
+  has_many :artworks, foreign_key: :created_on
+  # Validations
+  validates_presence_of :name, :email, :password_digest
 end
