03415c1a96bc81b6b0458d397a4c489580e7d194
Franco Rivera <francoriverarivas@hotmail.com>
2018-06-19 13:52:53 -0500

    added some routes, updated user controller to return all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
+  before_action :set_user, only: [:show, :update, :destroy]
+
   # POST /signup
   # return authenticated token upon signup
   def create
@@ -8,6 +10,19 @@ class UsersController < ApplicationController
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
     json_response(response, :created)
+  end
+
+  # GET /users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+
+  # GET /users/:id/artworks
+  def artworks
+    @artworks = user
+    json_response(@artworks)
   end
 
   private
@@ -19,5 +34,9 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :artworks do
   end
+  get 'users', to: 'users#index'
+  get 'users/:id/artworks', to: 'users#artworks'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
